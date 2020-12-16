9b04c2f2efad437fac0c87beb501ef71a3970d53
Colin Whitten <csw98@cornell.edu>
2020-12-16 13:28:45 -0500

    pair training data

--- sytems_for_training/2siestatonpy.py
+++ sytems_for_training/2siestatonpy.py
@@ -1,10 +1,10 @@
 import dpdata
 
 
-my_system = dpdata.LabeledSystem('test.out',fmt = 'siesta/aimd_output')
+my_system = dpdata.LabeledSystem('PAH3boundedfattyacid.out',fmt = 'siesta/aimd_output')
 
 
 
-my_out = my_system.to('deepmd/raw', 'dpmd_raw')
+my_out = my_system.to('deepmd/npy', 'dpmd_npy')
 
 
